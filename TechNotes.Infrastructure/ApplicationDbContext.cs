252f7a538ed25f03dfc06f96252b686db9337fb9
Abner-J-Hdz <48656614+Abner-J-Hdz@users.noreply.github.com>
2026-03-22 14:50:39 -0600

    Add ApplicationDbContext and EF Core references
    Introduce ApplicationDbContext inheriting from DbContext with a Notes DbSet and constructor accepting DbContextOptions. Update Infrastructure project to reference Microsoft.EntityFrameworkCore (8.0.20) and add a ProjectReference to TechNotes.Domain so the context can use domain entities.

---
+++ TechNotes.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TechNotes.Domain.Entities;
+
+namespace TechNotes.Infrastructure;
+public class ApplicationDbContext: DbContext
+{
+	public ApplicationDbContext(DbContextOptions options): base(options)
+	{
+		
+	}
+
+	public DbSet<Notes> Notes { get; set; }
+
+}
